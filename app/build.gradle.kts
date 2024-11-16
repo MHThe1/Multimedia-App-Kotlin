65fbe2991d6c3e92b27cec9bb4a28d8e13140732
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-11-17 04:21:04 +0600

    video player support added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,8 @@ dependencies {
 
     // Jetpack Compose integration
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-exoplayer-dash:1.4.1")
+    implementation("androidx.media3:media3-ui:1.4.1")
 }
