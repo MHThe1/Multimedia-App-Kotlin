a8e49a7b75dc8d3040878c1e95d003e8be2d23b8
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-11-16 17:19:44 +0600

    NavDrawer with nav drawer items added

--- app/src/main/java/com/classwork/multimediaapp/MainActivity.kt
+++ app/src/main/java/com/classwork/multimediaapp/MainActivity.kt
@@ -5,19 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -28,6 +35,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.classwork.multimediaapp.ui.theme.MultimediaAppTheme
 import kotlinx.coroutines.launch
 
@@ -73,7 +81,80 @@ fun Screen(modifier: Modifier = Modifier) {
 
 @Composable
 fun DrawerContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Multimedia App",
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp)
+    )
+    HorizontalDivider()
 
+    Spacer(modifier = Modifier.height(6.dp))
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Build,
+                contentDescription = "Broadcast Receiver"
+            )
+        },
+        label = { Text(
+            text = "Broadcast Receiver",
+            fontSize = 16.sp,
+        ) },
+        selected = false,
+        onClick = { /*TODO*/ },
+    )
+
+    Spacer(modifier = Modifier.height(6.dp))
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Face,
+                contentDescription = "Image Scale"
+            )
+        },
+        label = { Text(
+            text = "Image Scale",
+            fontSize = 16.sp,
+        ) },
+        selected = false,
+        onClick = { /*TODO*/ },
+    )
+
+    Spacer(modifier = Modifier.height(6.dp))
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.PlayArrow,
+                contentDescription = "Video"
+            )
+        },
+        label = { Text(
+            text = "Video",
+            fontSize = 16.sp,
+        ) },
+        selected = true,
+        onClick = { /*TODO*/ },
+    )
+
+    Spacer(modifier = Modifier.height(6.dp))
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Call,
+                contentDescription = "Audio"
+            )
+        },
+        label = { Text(
+            text = "Audio",
+            fontSize = 16.sp,
+        ) },
+        selected = false,
+        onClick = { /*TODO*/ },
+    )
 }
 
 @Composable
